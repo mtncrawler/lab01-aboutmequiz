82767eb63498193b8550e3195388b970f2b3a306
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-09 09:53:19 -0700

    added question1

--- lab01-aboutmetquiz/lab01-aboutmetquiz/Program.cs
+++ lab01-aboutmetquiz/lab01-aboutmetquiz/Program.cs
@@ -6,7 +6,30 @@ namespace lab01_aboutmetquiz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to my About Me Quiz!");
+            Question1();
+            Question2();
         }
+
+        static void Question1()
+        {
+            Console.WriteLine("How many siblings do I have?");
+            int answer1 = Int32.Parse(Console.ReadLine());
+            if(answer1 == 2)
+            {
+                Console.WriteLine("You are correct!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong! I have 2 siblings.");
+            }
+
+        }
+
+        static void Question2()
+        {
+
+        }
+
     }
 }
