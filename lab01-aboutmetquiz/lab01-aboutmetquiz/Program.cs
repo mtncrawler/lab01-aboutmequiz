bbdfef436c8d3daf2bdb985bc95aa6d742b9eca8
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-09 10:19:13 -0700

    made up to question 4; still need try/catch

--- lab01-aboutmetquiz/lab01-aboutmetquiz/Program.cs
+++ lab01-aboutmetquiz/lab01-aboutmetquiz/Program.cs
@@ -7,15 +7,32 @@ namespace lab01_aboutmetquiz
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to my About Me Quiz!");
-            Question1();
+            Console.WriteLine(Question1());
             Question2();
+            Console.WriteLine($"The answer is {Question3()}!");
+            Question4();
         }
 
-        static void Question1()
+        static string Question1()
         {
             Console.WriteLine("How many siblings do I have?");
             int answer1 = Int32.Parse(Console.ReadLine());
             if(answer1 == 2)
+            {
+                return "You are correct!";
+            }
+            else
+            {
+                return "Wrong! I have 2 siblings.";
+            }
+
+        }
+
+        static void Question2()
+        {
+            Console.WriteLine("What area of South America do I want to visit?");
+            string answer2 = Console.ReadLine();
+            if (answer2 == "Patagonia" || answer2 == "patagonia")
             {
                 Console.WriteLine("You are correct!");
             }
@@ -23,13 +40,33 @@ namespace lab01_aboutmetquiz
             {
                 Console.WriteLine("Wrong! I have 2 siblings.");
             }
-
         }
 
-        static void Question2()
+        static bool Question3()
         {
-
+            Console.WriteLine("True or false: I have lived in 5 states.");
+            string answer3 = Console.ReadLine();
+            if (Boolean.Parse(answer3))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
+        static void Question4()
+        {
+            Console.WriteLine("What month was I born?");
+            string answer4 = Console.ReadLine();
+            if(answer4 == "October" || answer4 == "october")
+            {
+                Console.WriteLine("You are correct!");
+            } else
+            {
+                Console.WriteLine("Wrong! I was born in October.");
+            }
+        }
     }
 }
